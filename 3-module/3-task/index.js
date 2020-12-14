912e7da7948fcb6302b125d9ef0ce8ce560dddeb
Nikolay Makarevich <nikoladze529@mail.ru>
2020-12-14 22:19:33 +0300

    Task 3.
    Writed function to translate a string to camelCase.
    Added extra 2 versions of solution.

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,5 +3,42 @@
  * @returns {string}
  */
 function camelize(str) {
-  // ваш код...
+  let arrStr = str.split("-");
+
+  for (let i = 1; i < arrStr.length; i++) {
+    arrStr[i] = arrStr[i][0].toUpperCase() + arrStr[i].slice(1);
+  }
+
+  return arrStr.join("");
 }
+
+//Вариант с использованием map:
+/*
+function camelize(str) {
+  let arrStr = str.split("-");
+
+  let arrStrCamel = ( arrStr.map(item => 
+    (item[0].toUpperCase() + item.slice(1))) ).join("");
+  
+  return arrStrCamel[0].toLowerCase() + arrStrCamel.slice(1);
+}
+*/
+
+//Вариант с использованием функции ucFirst и map:
+/*
+function camelize(str) {
+  let arrStr = str.split("-");
+
+  let ucFirst = function (str) {
+    if (!str) {
+      return str;
+    }
+
+    return str[0].toUpperCase() + str.slice(1);
+  }
+
+  let arrStrCamel = ( arrStr.map(item => ucFirst(item))).join("");
+
+  return arrStrCamel[0].toLowerCase() + arrStrCamel.slice(1);
+}
+*/
