d94f000305b5a1d81ded7f6a8e211f1437465ab9
Nikolay Makarevich <nikoladze529@mail.ru>
2021-01-13 10:55:02 +0300

    Module 6
    Task 1 is done.

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -30,5 +30,54 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this._rows = rows;
+    this.elem = document.createElement('table');
+    this.renderTable();
   }
+
+  renderTable() {
+    this.elem.innerHTML = `${tableHeaderTemplate()}
+          ${tableBodyTemplate(this._rows)}`;
+
+    this.elem.addEventListener('click', (event) => {
+      const target = event.target;
+
+      if (target.tagName !== 'BUTTON') {
+        return;
+      }
+
+      const tr = target.closest('tr');
+      tr.remove();
+    })
+  }
+}   
+  
+function tableHeaderTemplate() {
+  return `<thead>
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr>
+    </thead>`
+}  
+
+function tableBodyTemplate(items = []) {
+  return `<tbody>
+      ${items.map(item => `<tr>${tableCellsTemplate(item)}${deleteTableRowButton()}</tr>`).join('')}
+    </tbody>`;
+}
+
+function tableCellsTemplate(item = {}) {
+  let cells = '';
+  for (const key in item) {
+    cells += `<td>${item[key]}</td>`
+  }
+  return cells;
+}
+
+function deleteTableRowButton() {
+  return `<td><button>X</button></td>`
 }
