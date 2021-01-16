b0d0bb359412459ea5189cb59e1bc5173d73d09b
Nikolay Makarevich <nikoladze529@mail.ru>
2021-01-16 20:18:13 +0300

    Module 7. Task 1 and Task 2 are done.
    Task 2 have some problems

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,5 +2,61 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+    this._modal = null;
+    this._title = null;
+    this._modalBody = null;
+
+    this._keydown = this._keydown.bind(this);
   }
+
+  open() {
+    this._modal = createElement(modalTemplate(this._title, this._modalBody));
+    document.body.append(this._modal);
+    document.body.classList.add('is-modal-open');
+
+    this._modalCloseButton.addEventListener('click', this.close);
+    document.addEventListener('keydown', this._keydown);
+  }
+
+  close() {
+    document.body.classList.remove('is-modal-open');
+    document.body.querySelector('.modal').remove();
+  }
+
+  setTitle(title) {
+    this._title = title;
+  }
+
+  setBody(modalBody) {
+    this._modalBody = modalBody.innerHTML;
+  }
+
+  _keydown(event) {
+    if(event.code === `Escape`) {
+      this.close();
+    }
+  }
+
+  get _modalCloseButton() {
+    return this._modal.querySelector('.modal__close');
+  }
+}
+
+function modalTemplate(title, body){
+  return `<div class="modal">
+    <div class="modal__overlay"></div>
+    <div class="modal__inner">
+      <div class="modal__header">
+        <button type="button" class="modal__close">
+          <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+        </button>
+        <h3 class="modal__title">
+          ${title}
+        </h3>
+      </div>
+      <div class="modal__body">
+        ${body}
+      </div>
+    </div>
+  </div>`
 }
