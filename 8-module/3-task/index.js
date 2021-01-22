7ac1ea0b2c5996f401141043bffc0eb5da3aea53
Nikolay Makarevich <nikoladze529@mail.ru>
2021-01-22 19:45:04 +0300

    Task 3 is done

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,23 +6,59 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    const productItem = {
+      product: {},
+      count: 1
+    };
+
+    let cartItem = {};
+
+    const itemNames = this.cartItems.map( item => item.product.name );
+    const index = itemNames.indexOf(product.name);
+
+    if ( index < 0 || this.isEmpty()) {
+      Object.assign(productItem.product, product);
+      this.cartItems.push(productItem); 
+      cartItem = productItem;
+    }
+    else {
+      this.cartItems[index].count++;
+      cartItem = this.cartItems[index];
+    }
+
+    this.onProductUpdate(this.cartIcon);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    const itemIds = this.cartItems.map( item => item.product.id );
+    const index = itemIds.indexOf(productId);
+
+    this.cartItems[index].count += amount;
+
+    if ( this.cartItems[index].count == 0 ) {
+      this.cartItems.splice(index, 1);
+    }
+
+    this.onProductUpdate(this.cartIcon);
   }
 
   isEmpty() {
-    // ваш код
+    return !Boolean(this.cartItems.length);
   }
 
   getTotalCount() {
-    // ваш код
+    let totalCount = 0;
+    this.cartItems.forEach( item => totalCount += item.count);
+    return totalCount;
   }
 
   getTotalPrice() {
-    // ваш код
+    let totalPrice = 0;
+    this.cartItems.forEach( item => {
+      totalPrice += item.product.price * item.count;
+    })
+
+    return totalPrice;
   }
 
   onProductUpdate(cartItem) {
