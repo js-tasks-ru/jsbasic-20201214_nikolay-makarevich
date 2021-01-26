77e551e6514a90686c2a5b029e70c1c47765f3e1
Nikolay Makarevich <nikoladze529@mail.ru>
2021-01-26 22:39:31 +0300

    Changes according teacher recommendations

--- 9-module/2-task/index.js
+++ 9-module/2-task/index.js
@@ -13,27 +13,23 @@ import Cart from '../../8-module/4-task/index.js';
 export default class Main {
 
   constructor() {
-    this.carousel = new Carousel(slides);
-    this.ribbonMenu = new RibbonMenu(categories);
-    this.stepSlider = new StepSlider( {steps: 5, value: 3} );
-    this.cartIcon = new CartIcon();
-    this.cart = new Cart(this.cartIcon);
+    this.carousel = null;
+    this.ribbonMenu = null;
+    this.stepSlider = null;
+    this.cartIcon = null;
+    this.cart = null;
   }
 
   async render() {
+    this.renderCarousel();
+    this.renderRibbonMenu();
+    this.renderStepSlider();
+    this.renderCartIcon();
+
+    this.cart = new Cart(this.cartIcon);
     
-    document.querySelector('[data-carousel-holder]').append(this.carousel.elem);
-    document.querySelector('[data-ribbon-holder]').append(this.ribbonMenu.elem);
-    document.querySelector('[data-slider-holder]').append(this.stepSlider.elem);
-    document.querySelector('[data-cart-icon-holder]').append(this.cartIcon.elem);
-
-    const response = await fetch('./products.json');
-    this.products = await response.json();
-    this.productsGrid = new ProductsGrid(this.products);
-    this.productsGridHolder = document.querySelector('[data-products-grid-holder]');
-    this.productsGridHolder.innerHTML = '';
-    this.productsGridHolder.append(this.productsGrid.elem);
-
+    await this.renderProducts();
+    
     this.productsGrid.updateFilter({
       noNuts: document.getElementById('nuts-checkbox').checked,
       vegeterianOnly: document.getElementById('vegeterian-checkbox').checked,
@@ -42,6 +38,35 @@ export default class Main {
     })
 
     this.addEventListeners();
+  }
+
+  renderCarousel() {
+    this.carousel = new Carousel(slides);
+    document.querySelector('[data-carousel-holder]').append(this.carousel.elem);
+  }
+
+  renderRibbonMenu() {
+    this.ribbonMenu = new RibbonMenu(categories);
+    document.querySelector('[data-ribbon-holder]').append(this.ribbonMenu.elem);
+  }
+
+  renderStepSlider() {
+    this.stepSlider = new StepSlider( {steps: 5, value: 3} );
+    document.querySelector('[data-slider-holder]').append(this.stepSlider.elem);
+  }
+
+  renderCartIcon() {
+    this.cartIcon = new CartIcon();
+    document.querySelector('[data-cart-icon-holder]').append(this.cartIcon.elem);
+  }
+
+  async renderProducts() {
+    const response = await fetch('./products.json');
+    this.products = await response.json();
+    this.productsGrid = new ProductsGrid(this.products);
+    this.productsGridHolder = document.querySelector('[data-products-grid-holder]');
+    this.productsGridHolder.innerHTML = '';
+    this.productsGridHolder.append(this.productsGrid.elem);
   }
 
   addEventListeners() {
